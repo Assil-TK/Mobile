8826ace3a500a4288f00b130704a24ce8dfd9477
Assil Tekaya <tekayaassil8@gmail.com>
2024-12-02 20:17:53 +0100

    flatlist

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import products from './src/data/products';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World, this is Assil</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.productContainer}>
+            <Image source={{ uri: item.image }} style={styles.image} />
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.price}>${item.price}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -16,5 +26,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  productContainer: {
+    marginBottom: 20,
+    alignItems: 'center',
+  },
+  image: {
+    marginTop: 70,
+    width: 200,
+    height: 200,
+  },
+  name: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  price: {
+    fontSize: 14,
+    color: 'gray',
   },
 });
---
+++ src/data/products.js
@@ -0,0 +1,8 @@
+const products = [
+    { id: '1', name: 'Nike Shoes', price: 100, image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png' },
+    { id: '2', name: 'Adidas Shoes', price: 120, image: 'https://example.com/adidas.png' },
+    // Ajoutez d'autres produits ici
+  ];
+  
+  export default products;
+  
